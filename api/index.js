7246b4938ab041a7b103e718b93b4b90f39a4f03
mrmicaiah <mrmicaiah@gmail.com>
2026-02-03 15:49:07 -0500

    Trigger Cloudflare deploy after disabling GitHub Actions

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 /**
  * Courier - Email Marketing Platform
  * Cloudflare Worker + D1 Database + AWS SES
- * Deployed: 2026-02-03
+ * Deployed via Cloudflare Git Integration
+ * Last deploy trigger: 2026-02-03 20:50 UTC
  */
 
 import { checkAuth, getCorsHeaders, jsonResponse, CORS_HEADERS } from './lib.js';
